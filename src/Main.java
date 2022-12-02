94d31278cc605bdf9dd76b9d9d0cf65be20ae5c5
jalig <117361652+jalig@users.noreply.github.com>
2022-12-02 11:15:59 +0400

    Восклицание!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, меня зовут Тигран");
+        System.out.println("Привет, меня зовут Тигран!");
     }
 }
